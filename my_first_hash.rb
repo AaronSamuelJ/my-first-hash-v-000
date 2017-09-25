fa373363f2ec1acffbe13d9ea82b96336861cf77
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 18:49:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,3 +1,6 @@
-def my_hash 
+def my_hash
   {"dong-size" => 12, "girfriend's pussy" => "tight"}
+end
+def shipping_manifest
+  shipping_manifest = {"whale bone corsets" => 12, "oil paintings" => 3, "porcelain vases" => 10}
 end
